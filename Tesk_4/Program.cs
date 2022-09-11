1c90451b8fb6f590d903bda562512bceb1f3cc63
Anna_Belousova <anna-stepan_90@bk.ru>
2022-09-11 19:53:13 +0500

    Tesk_8

---
+++ Tesk_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число ");
+int namber = Convert.ToInt32(Console.ReadLine());
+
+    for (int i = 2; i <= namber; i++)
+        if (i % 2 == 0)
+        {
+            Console.Write(i);
+        }
+        
+        
+    
+    
