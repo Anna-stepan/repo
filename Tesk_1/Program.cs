4a851c25b82da88a73d43e31ab2b6eed4a67d09d
Anna_Belousova <anna-stepan_90@bk.ru>
2022-09-11 18:02:15 +0500

    Tesk_1

---
+++ Tesk_1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.Write("Введите 1 число ");
+int namber1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 2 число ");
+int namber2 = Convert.ToInt32(Console.ReadLine());
+
+    if (namber1 > namber2) 
+    {
+        Console.Write("Первое больше второго");
+    }
+    
+    else if (namber1 < namber2) 
+    {
+        Console.Write("Второе больше первого");
+    }
+    else 
+    {
+        Console.Write("числа равны");
+    }
+    
+
